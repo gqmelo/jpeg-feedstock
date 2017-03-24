10283c4f22e6ce0d0c13ebb44c1c7d37f67c805a
Guilherme Quentel Melo <gqmelo@gmail.com>
2017-03-24 19:44:49 -0300

    Use clcache

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,12 +2,26 @@ REM Configure step
 copy jconfig.vc jconfig.h
 if errorlevel 1 exit 1
 
+set CC=clcache
+set CXX=clcache
+clcache -M 104857600
+set "CLCACHE_BASEDIR=%SRC_DIR%
+clcache -z
+
+
 REM Build step
 cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release .
 if errorlevel 1 exit 1
 
+echo "################"
+echo %CC%
+echo %CXX%
+clcache -s
+echo "################"
+
 nmake
 if errorlevel 1 exit 1
+clcache -s
 
 REM Install step
 nmake install
